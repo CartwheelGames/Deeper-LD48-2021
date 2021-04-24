7d721092c29d45d9199176bca11882cf771927e2
Michael Consoli <michael@cartwheelgames.com>
2021-04-24 10:31:58 -0400

    Logic added to GameManager

--- Assets/Scripts/Logic/GameManager.cs
+++ Assets/Scripts/Logic/GameManager.cs
@@ -1,8 +1,70 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public sealed class GameManager : MonoBehaviour
 {
-    
+	[SerializeField]
+	private Sequence _sequence;
+	[SerializeField]
+	private BasicEvent OnStartGame;
+	[SerializeField]
+	private BasicEvent OnEndGame;
+	[SerializeField]
+	private CardEvent OnChangeCard;
+
+	private Card _currentCard;
+	private int _nextCardIndex;
+	private int _phaseIndex;
+	private int _score;
+
+	public void Start()
+	{
+		Next();
+		OnStartGame.Invoke();
+	}
+
+	public void Next()
+	{
+		if (TryGetCardPhase(out _currentCard))
+		{
+			OnChangeCard.Invoke(_currentCard);
+		}
+		else
+		{
+			OnEndGame.Invoke();
+		}
+	}
+
+	private bool TryGetCardPhase(out Card card)
+	{
+		while (_phaseIndex < _sequence.Count)
+		{
+			Phase phase = _sequence.GetPhaseAt(_phaseIndex);
+			if (TryGetNextValidCardPhase(phase, out card))
+			{
+				return true;
+			}
+			_phaseIndex++;
+		}
+		card = null;
+		return false;
+	}
+
+	private bool TryGetNextValidCardPhase(Phase phase, out Card card)
+	{
+		while (_nextCardIndex < phase.Cards.Count)
+		{
+			card = phase.Cards[_nextCardIndex++];
+			if (_score >= card.MinPoints && _score <= card.MaxPoints)
+			{
+				return true;
+			}
+		}
+		card = null;
+		return false;
+	}
+
+	private void EndGame()
+	{
+
+	}
 }
