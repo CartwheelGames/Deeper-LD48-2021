df67f18bc1fc1176f8a39f36b9983ff30e70b38c
Kirsten Sugar <kirsten.sugar@weplaydots.com>
2021-04-24 17:21:28 -0400

    anims and cards and butt stuff

---
+++ Assets/Scripts/UI/CardUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CardUI : MonoBehaviour {
+
+    public Sprite CardBackImage { set => _cardBackImage.sprite = value; }
+    public string Description { set => _descriptionText.text = value; }
+    
+    [SerializeField] private SpriteRenderer _cardBackImage;
+    [SerializeField] private TextMeshPro _descriptionText;
+    [SerializeField] private Animator _animator;
+
+    public void Flip(bool isLeft){
+        PlayAnim(isLeft ? "FlipLeft" : "FlipRight");
+    }
+
+    private void PlayAnim(string anim){
+        _animator.CrossFade(anim, 0.5f);
+    }
+}
